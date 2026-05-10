31bd07cd8658af3a905488c846bf410e319ce377
williamfurquim <w.furquim@aluno.senai.br>
2026-05-10 13:33:41 -0300

    style: layout complementation

---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,42 @@
+import { Link, useNavigate } from "react-router-dom"
+
+const SideBar = () => {
+    const navigate = useNavigate();
+
+    function logout() {
+        const confirmed = confirm('Tem certeza que quer desconectar?');
+        if (confirmed){
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            navigate('/login');
+        } else {
+            return
+        }
+    }
+
+    return (
+        <aside>
+            <h2 className="logo">Revise.e</h2>
+
+            <nav>
+                <ul>
+                    <li><Link to={'/notas'}>Anotações</Link></li>
+                    <li><Link to={'/configuracoes'}>Configurações</Link></li>
+                    <li><a href="https://github.com/williamfurquim/Revisee"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        {/* Evita vulnerabilidade de segurança */}
+                        GitHub
+                    </a></li>
+                    <li onClick={logout}>
+                        <button>
+                            Desconectar
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
+
+export default SideBar
---
+++ frontend/src/pages/Configs.tsx
@@ -0,0 +1,21 @@
+import SideBar from '../components/SideBar'
+
+const Configs = () => {
+    return (
+        <div className='layout'>
+            <SideBar></SideBar>
+
+            <main>
+                <button style={{ marginRight: 10 }}>Alterar: modo claro</button>
+                <button style={{ marginRight: 10 }}>Trocar idioma</button>
+
+                <button style={{ marginRight: 10 }}>Alterar E-mail</button>
+                <button style={{ marginRight: 10 }}>Alterar senha</button>
+
+                
+            </main>
+        </div>
+    )
+}
+
+export default Configs
