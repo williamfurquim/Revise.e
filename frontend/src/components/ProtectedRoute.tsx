b63d8f92e549cd977a7737659611334a6e5a662c
williamfurquim <w.furquim@aluno.senai.br>
2026-05-10 13:36:40 -0300

    Create ProtectedRoute

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children
+}: any) {
+
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
