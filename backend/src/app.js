4c1ed581eb7968064e7cc445cfac31c565b40e63
williamfurquim <w.furquim@aluno.senai.br>
2026-05-17 21:11:01 -0300

    prepare render deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,9 +14,15 @@ app.use(express.json({
     limit: "10kb"
 }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "Deploy Ok"
+    });
+});
 
 app.use('/api/auth', authRouter);
 app.use('/api', notesRouter);
