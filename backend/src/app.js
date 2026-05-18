6b84c3ab6efe5705e410da71590d7cf4584b816e
williamfurquim <w.furquim@aluno.senai.br>
2026-05-17 22:10:32 -0300

    fix cors production

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,11 +13,22 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({
     limit: "10kb"
 }));
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+];
+
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        process.env.FRONTEND_URL
-    ],
+    origin: (origin, callback) => {
+        console.log("Origin:", origin);
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
 
